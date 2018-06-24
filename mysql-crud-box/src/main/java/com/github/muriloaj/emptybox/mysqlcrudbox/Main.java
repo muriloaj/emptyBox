da2b469087ff425d899ab8bbc0895be102a39bce
MJ <muriloaj@gmail.com>
2018-06-23 21:16:51 -0300

    hello world gradle + spark

--- mysql-crud-box/src/main/java/com/github/muriloaj/emptybox/mysqlcrudbox/Main.java
+++ mysql-crud-box/src/main/java/com/github/muriloaj/emptybox/mysqlcrudbox/Main.java
@@ -4,7 +4,7 @@ import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        get("/hello/:name", (req, res) -> "Hello World ".concat(req.params("name")));
     }
 }
 
